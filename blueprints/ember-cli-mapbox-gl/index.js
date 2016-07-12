c05cdf8bf454ff6f0d88493e7664a647d1fe7706
Simon Garcia <simon.garciar@gmail.com>
2016-07-12 23:02:50 +0200

    Update for ember-cli 2.5.1

--- blueprints/ember-cli-mapbox-gl/index.js
+++ blueprints/ember-cli-mapbox-gl/index.js
@@ -2,10 +2,5 @@
 
 module.exports = {
   normalizeEntityName: function() {},
-
-  afterInstall: function() {
-    return this.addBowerPackagesToProject([
-      { name: "mapbox-gl-js", target: "0.11.0" }
-    ]);
   }
 };
